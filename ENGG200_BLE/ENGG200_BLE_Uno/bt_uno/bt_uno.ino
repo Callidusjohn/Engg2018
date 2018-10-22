ccdbf62eb9b75e46a50ac68db38f79396a7e065f
bradleykenny <bkenny283@gmail.com>
2018-10-22 18:16:27 +1100

    Comments left from git merge.

--- ENGG200_BLE/ENGG200_BLE_Uno/bt_uno/bt_uno.ino
+++ ENGG200_BLE/ENGG200_BLE_Uno/bt_uno/bt_uno.ino
@@ -1,12 +1,5 @@
 #include <SoftwareSerial.h>
-<<<<<<< HEAD
-#include "bluetooth_uno.cpp"
-
-//AltSoftSerial BTSerial;
-bluetooth_uno bt;
-=======
 #include "bluetooth_uno.h"
->>>>>>> 2cf69718a14ab75bae90f3faa00ea28eef419ec5
 
 void setup() {
   	BluetoothUno.initiateConnToMega();
