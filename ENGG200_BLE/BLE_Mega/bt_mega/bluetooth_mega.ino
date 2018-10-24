08926c7fe1b01d70fe3294e5a58a6c88389d1c84
Davis Erwin <davis.erwin@students.mq.edu.au>
2018-10-24 11:04:49 +1100

    updated mega

--- ENGG200_BLE/BLE_Mega/bt_mega/bluetooth_mega.ino
+++ ENGG200_BLE/BLE_Mega/bt_mega/bluetooth_mega.ino
@@ -1,21 +1,31 @@
-#include <SoftwareSerial.h>
 
-bool initiateConnToUno() {
-	int PIN = 9;
-	pinMode(PIN, INPUT);
-	Serial.begin(9600);
-	Serial.print("AT"); // this should print OK to monitor
-	Serial.write("AT+nnnnnnnn"); // replace n with MAC address of Uno
+#include <Arduino.h>
+#include "interpretData.h"
+#include "shared_types.h"
+
+char c;
+
+//CanQuantities data = {r, g, b};
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial2.begin(9600);
+  //Serial2.println("conn
+
 }
 
-void receiveDataFromUno() {
-	char c; // replace with data type
+void loop() {
+  // put your main code here, to run repeatedly:
+    if(Serial2.available()){
+    char c = Serial2.read();
+    char r = interpretRed(c);
+    char g = interpretGreen(c);
+    char b = interpretBlue(c);
+    CanQuantities quantity = {r, g, b};
+    Serial.println(quantity.red);
+    Serial.println(quantity.green);
+    Serial.println(quantity.blue);
+    Serial.println("");
+    }
 
-	if (Serial.available()) {
-		c = Serial.read();
-		Serial.write(c);
-	}
-	if (c > 0) {
-		// call other functions here
-	}
 }
